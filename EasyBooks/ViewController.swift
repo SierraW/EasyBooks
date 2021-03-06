67d8078421d929181915c621764b11ee569f4b6e
Yiyao Zhang <me@yiyao.ca>
2021-03-05 22:30:10 -0500

    Initial Commit

---
+++ EasyBooks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBooks
+//
+//  Created by Yiyao Zhang on 2021-03-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
